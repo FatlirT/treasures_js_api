db1b8b51084e4f749730d871d8edad8c6ae05bd2
Fatlir Topalli <fatlir-t@outlook.com>
2024-02-16 14:33:45 +0000

    add solutions and tests for task 7

--- models/treasures.model.js
+++ models/treasures.model.js
@@ -3,23 +3,28 @@ const db = require(`${__dirname}/../db`);
 exports.selectAllTreasures = (sort_by = "age", colour, order = "ASC") => {
     const validColumns = [
         "treasure_id",
-		"treasure_name",
-		"colour",
-		"age",
-		"cost_at_auction",
-		"shop_name",
-	];
-    
-	if (!validColumns.includes(sort_by)) {
+        "treasure_name",
+        "colour",
+        "age",
+        "cost_at_auction",
+        "shop_name",
+    ];
+
+    if (!validColumns.includes(sort_by)) {
         throw { status: 400, msg: "Invalid order query" };
-	}
-	if (order !== "ASC" && order !== "DESC") {
+    }
+    if (order !== "ASC" && order !== "DESC") {
         throw { status: 400, msg: "Invalid order query" };
-	}
-    colour = colour ? colour: "invalid_colour";
-    // const colourUsed = colour ? "WHERE treasures.colour = $1": "";
-    console.log("We got into MODEL");
-	return db.query(
+    }
+
+    const colourFilter =
+        colour !== undefined ? `WHERE treasures.colour = $1` : "";
+
+    const dbVars = [];
+    if (colour !== undefined) {
+        dbVars.push(colour);
+    }
+    return db.query(
         `
         SELECT
         treasures.treasure_id,
@@ -32,7 +37,10 @@ exports.selectAllTreasures = (sort_by = "age", colour, order = "ASC") => {
         treasures
     JOIN
         shops ON treasures.shop_id = shops.shop_id
+    ${colourFilter}
     ORDER BY treasures.${sort_by} ${order}
-    WHERE treasures.colour = $1
-`, [colour] );
+    
+`,
+        dbVars
+    );
 };
